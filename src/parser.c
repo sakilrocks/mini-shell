0d8e50516b1060b9f7d464fcd6d9482e9ec93dfe
sakil <147094431+sakilrocks@users.noreply.github.com>
2025-09-20 23:16:45 +0530

    code parser.c

--- src/parser.c
+++ src/parser.c
@@ -1,1 +1,14 @@
+#include <string.h>
+#include "shell.h"
 
+// parse input str into arr of args
+
+void parse_input(char *input, char **args) {
+	int i = 0;
+	char *token = strtok(input, " \t\n");
+	while (token != NULL && i < MAX_ARGS -1) {
+		args[i++] = token;
+		token = strtok(NULL, " \t\n");
+	}
+	args[i] = NULL;
+}
